3b3c3aa90cf640740672db1bfda93ab338b65298
SamirBou <speediebe@protonmail.com>
2020-09-30 23:17:56 -0400

    Update overlay

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -2,4 +2,12 @@
 
 include_controls 'couchbase-enterprise-srg-baseline' do
 
+    control "V-58137" do
+        impact 0.0
+        describe 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
+        include LDAP capabilities' do
+            skip 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
+            include LDAP capabilities'
+        end
+    end
 end
