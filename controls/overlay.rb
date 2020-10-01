af4a1afba55323744ce01a3fe67d45f4e4442293
Truong, Nhi T <ntruong@mitre.org>
2020-10-01 16:18:23 -0400

    Fixed git repo link

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -2,15 +2,6 @@
 
 include_controls 'couchbase-enterprise-srg-baseline' do
 
-  control "V-58137" do
-    impact 0.0
-    describe 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
-    include LDAP capabilities' do
-      skip 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
-      include LDAP capabilities'
-    end
-  end
-
   control "V-32347" do
     impact 0.0
     describe 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
@@ -136,4 +127,14 @@ include_controls 'couchbase-enterprise-srg-baseline' do
       include Auditing capabilities'
     end
   end 
+
+  control "V-58137" do
+    impact 0.0
+    describe 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
+    include LDAP capabilities' do
+      skip 'This control is not applicable on Couchbase Server Community, as the Community version of Couchbase does not 
+      include LDAP capabilities'
+    end
+  end
+
 end
